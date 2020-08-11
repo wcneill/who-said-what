191aee63c5d768548c0d67a4c308ed9adf8bbbef
Wesley Neill <wesley.neill@gmail.com>
2020-08-11 10:48:42 -0700

    added method bodies and unit tests

--- wsw/fingerprint.py
+++ wsw/fingerprint.py
@@ -6,31 +6,57 @@ def denoise(clip):
     pass
 
 
-def lpfilter(clip, sr):
-    pass
+def lpfilter(signal, sr):
+    cutoff = 10e3
+    sos = sig.butter(10, cutoff, fs=sr, btype='lowpass', analog=False, output='sos')
+    return sig.sosfilt(sos, signal)
 
 
-def downsample(clip, sr):
-    pass
+def downsample(signal, old_sr, new_sr):
+    return librosa.resample(signal, old_sr, new_sr)
 
 
-def stft(signal, window):
-    pass
+def stft(signal, N):
+    spec = librosa.stft(signal, n_fft=N, window=sig.windows.hamming)
+    return np.abs(spec)
 
 
-def ffilter(spec, bands=6):
-    pass
+def denoise(spectrogram):
+    return librosa.decompose.nn_filter(spectrogram)
 
 
-def spec_filter():
-    pass
+def log_bin(arr, n_bins):
+    bands = np.array([10 * 2 ** i for i in range(n_bins - 1)])
+    idxs = np.arange(len(arr))
+    split_arr = np.split(arr, np.searchsorted(idxs, bands))
+    return split_arr
+
+
+def ft_filter(ft, n_bins, alpha=1):
+    binned_ft = log_bin(ft, n_bins)
+    for i, band in enumerate(binned_ft):
+        band_max = np.max(band)
+        filtered = np.where(band < band_max, 0, band)
+        binned_ft[i] = filtered
+    joined_filtered = np.concatenate(binned_ft)
+    thresh = np.mean(joined_filtered)
+
+    return np.where(joined_filtered < thresh * alpha, 0, joined_filtered)
+
+
+def spec_filter(spec, n_bins):
+    filtered = np.zeros_like(spec.T)
+    for i, dft in enumerate(spec.T):
+        filtered[i] = ft_filter(dft, n_bins)
+    return filtered.T
 
 class Fingerprint():
-    def __init__(self, clip, sr, window):
-        self.clip = clip
+    def __init__(self, path, sr=22050, n_ftt=512):
+        y, sr = librosa.load(path, sr)
+        self.signal = y
         self.sr = sr
-        self.window = window
-        self.fingerprint = self.get_prints(self.clip, window)
+        self.n_ftt = 512
+        # self.fingerprint = self.get_prints(self.clip, window)
 
     # divide each FT into logarithmic bands and extract
     # Strongest frequencies. Return filtered transforms
