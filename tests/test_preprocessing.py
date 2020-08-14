e5c19dea56f537cdacbfa28f2ffd2bf044c21bec
Wesley Neill <wesley.neill@gmail.com>
2020-08-14 10:44:27 -0700

    added doc strings and failed assertion messages to unit test modules.

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -12,6 +12,11 @@ import shutil
 
 
 def test_write_audio():
+    """
+    Test to ensure audio file is written to disk in the expected location.
+    Ensure
+    :return:
+    """
     filename = librosa.ex('nutcracker')
     aud, sr = librosa.load(filename)
 
@@ -19,12 +24,19 @@ def test_write_audio():
     save_to = os.path.join(ROOT_DIR, rand_loc, 'test.wav')
 
     write_audio(save_to, aud, sr)
-    assert os.path.exists(save_to)
+    assert os.path.exists(save_to), "Expected path of saved file does not exist"
+    assert os.path.isfile(save_to), "File did not save successfully"
 
     shutil.rmtree(rand_loc)
 
 
 def test_resample():
+    """
+    Tests that a properly structured directory of labelled audio files is successfully
+    resampled at the desired rate, saved as a different file type, and written to
+    the desired new or old location.
+    :return:
+    """
     aud1, sr = librosa.load(librosa.ex('trumpet'))
     aud2, _ = librosa.load(librosa.ex('nutcracker'))
 
@@ -40,17 +52,16 @@ def test_resample():
     write_audio(save1, aud1, sr)
     write_audio(save2, aud2, sr)
 
-    # verify new files of type '.wav' save in same directory
+    # verify new files of type '.wav' save in same/old directory
     resample_all(rand_loc1, rand_loc1, sr)
-    assert os.path.isfile(os.path.join(sub_dir1, 'test1.wav'))
-    assert os.path.isfile(os.path.join(sub_dir2, 'test2.wav'))
-    # shutil.rmtree(rand_loc1)
+    assert os.path.isfile(os.path.join(sub_dir1, 'test1.wav')), "File not saved to old directory"
+    assert os.path.isfile(os.path.join(sub_dir2, 'test2.wav')), "File not saved to old directory"
 
     # verify new files of '.wav' are saved in new/different directory
     resample_all(rand_loc1, rand_loc2, sr)
-    assert os.path.isfile(os.path.join(rand_loc2, sub_dir1, 'test1.wav'))
-    assert os.path.isfile(os.path.join(rand_loc2, sub_dir2, 'test2.wav'))
+    assert os.path.isfile(os.path.join(rand_loc2, sub_dir1, 'test1.wav')), "File not saved to new directory"
+    assert os.path.isfile(os.path.join(rand_loc2, sub_dir2, 'test2.wav')), "File not saved to new directory"
 
-    # Delete all generated test files.
+    # Delete all generated test directories and files.
     shutil.rmtree(rand_loc1)
     shutil.rmtree(rand_loc2)
