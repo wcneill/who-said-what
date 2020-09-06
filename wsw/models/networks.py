cf4fe9b7d510ae46e26a0cd4d27e654f30dac5d6
Wesley Neill <wesley.neill@gmail.com>
2020-09-06 13:05:13 -0700

    changed first fully connected layer architecture to match network input of 200x200 pixel images

--- wsw/models/networks.py
+++ wsw/models/networks.py
@@ -20,9 +20,9 @@ class AVNN(nn.Module):
         self.conv2 = nn.Conv2d(64, 128, 7)
         self.conv3 = nn.Conv2d(128, 256, 5)
         self.conv4 = nn.Conv2d(256, 512, 3)
-        self.fc1 = nn.Linear(512 * 25 * 51, 1028)
+        self.fc1 = nn.Linear(512 * 18 * 18, 1028)
         self.fc2 = nn.Linear(1028, 512)
-        self.out = nn.Linear(512, 3)
+        self.out = nn.Linear(512, 4)  # classes: 0, 1, 2, or 3+ speakers
         self.pool = nn.MaxPool2d(2, 2)
         self.drop = nn.Dropout(p=0.2)
 
@@ -34,13 +34,13 @@ class AVNN(nn.Module):
         x = func.relu(self.conv3(x))
         x = self.drop(self.pool(x))
         x = func.relu(self.conv4(x))
-        x = x.view(-1, 512 * 25 * 51)
+        x = x.view(-1, 512 * 18 * 18)
         x = func.relu(self.fc1(x))
         x = func.relu(self.fc2(x))
         return self.out(x)
 
 
-def train_(model, epochs, lr, trainloader, validloader=None, plot=True):
+def train(model, epochs, lr, trainloader, validloader=None, plot=True):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = model.to(device)
 
@@ -58,7 +58,8 @@ def train_(model, epochs, lr, trainloader, validloader=None, plot=True):
         running_ac = 0
 
         for images, labels in trainloader:
-            images, labels = images.to(device), labels.to(device)
+            images = images.to(device)
+            labels = labels.to(device)
             opt.zero_grad()
             scores = model(images)
             t_loss = criterion(scores, labels)
@@ -68,12 +69,15 @@ def train_(model, epochs, lr, trainloader, validloader=None, plot=True):
             opt.step()
 
         model.train_loss.append(running_tl / len(trainloader))
+        if e % 10 == 0:
+            print(f'Epoch {e} Training Loss: {train_loss[-1]}')
 
         if validloader is not None:
             model.eval()
             with torch.no_grad():
                 for images, labels in validloader:
-                    images, labels = images.to(device), labels.to(device)
+                    images = images.to(device)
+                    labels = labels.to(device)
                     scores = model(images)
                     ps = func.log_softmax(scores, dim=1)
                     preds = torch.argmax(ps, dim=1)
@@ -85,24 +89,26 @@ def train_(model, epochs, lr, trainloader, validloader=None, plot=True):
             model.accuracy.apend(running_ac / len(validloader))
             model.train()
 
-        if plot:
-            fig, axes = plt.subplots(1, 2)
-            axes[0].title('Loss Plot')
-            axes[1].title('Accuracy')
-            axes[0].plot(train_loss, label='training')
-            axes[0].plot(valid_loss, label='validation')
-            axes[1].plot(accuracy)
-            axes[0].xlabel('Epochs')
-            axes[1].xlabel('Epochs')
-            plt.legend()
-            plt.show()
+            if e % 10 == 0:
+                print(f'Epoch {e} Validation Loss: {valid_loss[-1]}')
 
-
+    if plot:
+        fig, axes = plt.subplots(1, 2)
+        axes[0].title('Loss Plot')
+        axes[1].title('Accuracy')
+        axes[0].plot(train_loss, label='training')
+        axes[0].plot(valid_loss, label='validation')
+        axes[1].plot(accuracy)
+        axes[0].xlabel('Epochs')
+        axes[1].xlabel('Epochs')
+        plt.legend()
+        plt.show()
 
 
 if __name__ == '__main__':
+    # calculate last convlayer output shape (comment out FC layers first):
     network = AVNN()
-    im = torch.tensor(np.random.random((3, 257, 460)))
+    im = torch.tensor(np.random.random((3, 200, 200)))
     im = im.unsqueeze(0).float()
     out = network(im)
     print(im.shape)
